16c7951df07ea01dfdc6a7f188b26826741a6bc5
Aivar Z <aivarzh@MacBook-Air-Aivar.local>
2024-11-10 04:47:33 -0700

    Completed stages of tasks by using hashmaps instead of arraylists

--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        ToDo todolist = new ToDo();
+        ToDo todolist = new ToDo("Today tasks");
 
         todolist.addTask("Buy groceries");
         todolist.addTask("Complete homework");
         todolist.addTask("Play basketball");
-        todolist.removeTask(0);
+        todolist.displayTasks();
+        todolist.removeTask("Buy groceries");
+        todolist.markAsDone("Complete homework");
+        todolist.markAsDone("Him");
+        todolist.modifyTask("Complete homework", "Complete math");
 
         todolist.displayTasks();
     }
--- src/ToDo.java
+++ src/ToDo.java
@@ -1,21 +1,62 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ToDo {
-    private ArrayList<String> tasks = new ArrayList<String>();
+    private String topic;
+    private HashMap<String,Boolean> tasks;
 
-    public void addTask(String task){
-        tasks.add(task);
+    public ToDo(String topic){
+        this.topic = topic;
+        tasks = new HashMap<String,Boolean>();
     }
 
-    public void removeTask(int index){
-        tasks.remove(index);
+    public void addTask(String task){
+        tasks.put(task, false);
+    }
+
+    public void removeTask(String task){
+        if (tasks.containsKey(task)){
+            System.out.println("Removing " + task);
+            tasks.remove(task);
+        }
+        else {
+            System.out.println("No such task");
+        }
+    }
+
+    public void markAsDone(String task){
+        System.out.println("Marking " + task + " as completed");
+        if (tasks.containsKey(task)){
+            tasks.replace(task, true);
+        }
+        else {
+            System.out.println("No such task");
+        }
+    }
+
+    public void modifyTask(String oldTask, String newTask){
+        System.out.println("Modifying " + oldTask + " to " + newTask);
+        if (tasks.containsKey(oldTask)){
+            tasks.put(newTask, tasks.get(oldTask));
+            tasks.remove(oldTask);
+        }
+        else {
+            System.out.println("No such task");
+        }
     }
 
     public void displayTasks(){
-        for (int i = 0; i < tasks.size(); i++){
-            System.out.println(i + ". " + tasks.get(i));
+        if (tasks.size() == 0){
+            System.out.println("My ToDo list for " + topic + " is empty");
         }
-
-
+        else {
+            System.out.println("My tasks for " + topic);
+            for (String task : tasks.keySet()){
+                if (tasks.get(task)){
+                    System.out.println(task + " is completed.");
+                } else{
+                    System.out.println(task + " is not completed");
+                }
+            }
+        }
     }
 }
